f28fdb34503a915735b963eef44efc8062218205
Natasya Salsabilla <128036004+mixiii0006@users.noreply.github.com>
2025-05-13 19:19:31 +0700

    Add files via upload
    please finish

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
-
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -29,40 +27,11 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/sw.js', '**/service-worker.js'],
+          },
         },
       ],
     }),
-    // new GenerateSW({
-    //   clientsClaim: true,
-    //   skipWaiting: true,
-    //   runtimeCaching: [
-    //     {
-    //       urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-    //       handler: 'CacheFirst',
-    //       options: {
-    //         cacheName: 'images-cache',
-    //         expiration: {
-    //           maxEntries: 50,
-    //           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-    //         },
-    //       },
-    //     },
-    //     {
-    //       urlPattern: new RegExp('https://your-api-domain.com'),
-    //       handler: 'NetworkFirst',
-    //       options: {
-    //         cacheName: 'api-cache',
-    //         networkTimeoutSeconds: 10,
-    //         cacheableResponse: {
-    //           statuses: [0, 200],
-    //         },
-    //         expiration: {
-    //           maxEntries: 50,
-    //           maxAgeSeconds: 5 * 60, // 5 minutes
-    //         },
-    //       },
-    //     },
-    //   ],
-    // }),
   ],
 };
